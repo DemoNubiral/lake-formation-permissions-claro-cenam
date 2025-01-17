eac04e11c09a62ab8cf93b2c469b9f1ec6324bf6
Sergio Moreno C <sergio.moreno@nubiral.com>
2025-01-17 12:13:13 -0500

    MOD: se cambia yaml y script

--- code-permissions/permissions.py
+++ code-permissions/permissions.py
@@ -87,7 +87,7 @@ class Permissions:
 
 
 if __name__ == '__main__':
-    path_file = os.getenv("PATH_FILE")
+    path_file = os.getenv("PATH_FILES")
     print(path_file)
     data = open(path_file, 'r')
     read_data = data.read()
